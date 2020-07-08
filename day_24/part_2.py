3f8a30529fd3beac98bbd3848243a28fb14d9c79
Tim Ordo <towkep@inbox.ru>
2020-07-08 16:57:49 +0300

    Day 24 completed.

---
+++ day_24/part_2.py
@@ -0,0 +1,25 @@
+import itertools
+from typing import Iterable
+
+
+def multi(lst: Iterable):
+    answer = 1
+    for item in lst:
+        answer *= item
+    return answer
+
+
+answer_list = list()
+gifts_weight = list()
+with open("input.txt") as file:
+    for line in file.read().splitlines():
+        gifts_weight.append(int(line))
+
+group_weight = sum(gifts_weight) // 4
+
+for n in range(len(gifts_weight) // 4):
+    for package in itertools.combinations(gifts_weight, n):
+        if sum(package) == group_weight:
+            answer_list.append(multi(package))
+
+print(min(answer_list))
