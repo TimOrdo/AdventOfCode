2b8a1406a8fdfbdc6f944022d16d910b4ec066ad
TimOrdo <towkep@inbox.ru>
2021-07-03 15:38:19 +0300

    Day 6 of 2016 year completed.

---
+++ year2016/day_06/part_01.py
@@ -0,0 +1,22 @@
+import numpy as np
+import string
+
+input_list = []
+
+with open("input.txt") as file:
+    for line in file.read().splitlines():
+        input_list.append([x for x in line])
+
+transpose_matrix_input = np.transpose(np.array(input_list))
+answer = ''
+
+for item in transpose_matrix_input:
+    count = 0
+    password_letter = ''
+    for letter in string.ascii_lowercase:
+        if count < np.count_nonzero(item == letter):
+            count = np.count_nonzero(item == letter)
+            password_letter = letter
+    answer += password_letter
+
+print(answer)
